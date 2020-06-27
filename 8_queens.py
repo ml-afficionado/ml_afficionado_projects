4121aad4869a0c5b06fc98581f404ba251382943
ml-afficionado <57549337+ml-afficionado@users.noreply.github.com>
2020-06-27 20:08:44 +0530

    Create 8_queens.py
    This code helps in finding the non-attacking positions of 8 queens in a chess board.

---
+++ 8_queens.py
@@ -0,0 +1,53 @@
+#Number of queens
+N = 8
+
+#chessboard
+#NxN matrix with all elements 0
+board = [[0]*N for _ in range(N)]
+
+def is_attack(i, j):
+    #checking if there is a queen in row or column
+    for k in range(0,N):
+        if board[i][k]==1 or board[k][j]==1:
+            return True
+    #checking diagonals
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k+l==i+j) or (k-l==i-j):
+                if board[k][l]==1:
+                    return True
+    return False
+
+def N_queen(n):
+    #if n is 0, solution found
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            '''checking if we can place a queen here or not
+            queen will not be placed if the place is being attacked
+            or already occupied'''
+            if (not(is_attack(i,j))) and (board[i][j]!=1):
+                board[i][j] = 1
+                #recursion
+                #wether we can put the next queen with this arrangment or not
+                if N_queen(n-1)==True:
+                    return True
+                board[i][j] = 0
+
+    return False
+
+N_queen(N)
+def final_lst_solved():
+    final_lst=[]
+    for i in board:
+        #print (i)
+        index = i.index(1)
+        #print('index:',index)
+        final_lst.append(index)
+        #print('final_lst:',final_lst)
+        to_str = ' '.join([str(elem) for elem in final_lst])
+        #print(to_str) 
+    return to_str
+
+print('final_str',final_lst_solved())
